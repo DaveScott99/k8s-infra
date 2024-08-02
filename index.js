df551f6156d98b737beb4d888c817e3b9bafc2c3
DaveScott99 <101915085+DaveScott99@users.noreply.github.com>
2024-08-02 15:28:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send("Olá, " + req.hostname + " Teste Downtime");
+  res.send("Olá, " + req.hostname + " Teste CONTROLLER");
 });
 
 app.listen(3000, function (error) {
